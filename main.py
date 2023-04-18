7a6a7508726386930d9e745ca7465ee4f6978883
Daniils Petrovs <daniils@platogo.com>
2023-04-18 21:02:16 +0200

    Refactoring and improvements

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ auth_scheme = HTTPBearer()
 stub = modal.Stub(config.MODAL_STUB_NAME, image=app_image, secrets=[
                   modal.Secret.from_name(config.MODAL_SECRETS_NAME)])
 
+stub.in_progress = modal.Dict()
+
 logger = config.get_logger(__name__)
 volume = modal.SharedVolume().persist('vtscribe-cache-vol')
 
@@ -61,8 +63,11 @@ async def accept_job(request: Request, token: HTTPAuthorizationCredentials = Dep
     model_name = request.query_params.get('model_name', 'small.en')
 
     source = config.Source(source_id, source_url, source_type).validate()
+    job_spec = config.JobSpec(
+        source=source,
+        whisper_model=config.SUPPORTED_WHISPER_MODELS[model_name])
 
-    call = transcribe.spawn(source, model_name)
+    call = transcribe.spawn(job_spec)
     return {"call_id": call.object_id}
 
 
@@ -77,9 +82,8 @@ async def poll_results(call_id: str):
 
 
 @stub.function(timeout=40000, secret=modal.Secret.from_name("vtscribe-secrets"))
-async def transcribe(source: config.Source, model_name: str):
-    metadata = transcribe_vod.call(config.JobSpec(
-        source=source, whisper_model=config.SUPPORTED_WHISPER_MODELS[model_name]))
+async def transcribe(job_spec: config.JobSpec):
+    metadata = transcribe_vod.call(job_spec)
     return fastapi.responses.JSONResponse(metadata)
 
 
@@ -94,29 +98,43 @@ def transcribe_vod(job_spec: config.JobSpec):
         _type_: _description_
     """
 
+    import os
     import whisper
 
     source = job_spec.source
 
-    # pre-download the model to the cache path, because the _download fn is not
-    # thread-safe.
-    whisper._download(
-        whisper._MODELS[job_spec.whisper_model.name], config.MODEL_DIR, False)
+    try:
 
-    download_audio.call(job_spec.yt_video_url() if source.type ==
-                        'yt' else job_spec.twitch_video_url())
-    input_raw_file_path: str = f"{config.CACHE_DIR}/{source.id}.mp3"
-    result_metadata = do_transcribe.call(
-        input_raw_file_path, model=job_spec.whisper_model, result_path=f"{config.CACHE_DIR}/{source.id}-{source.type}-result.json")
+        # pre-download the model to the cache path, because the _download fn is not
+        # thread-safe.
+        whisper._download(
+            whisper._MODELS[job_spec.whisper_model.name], config.MODEL_DIR, False)
 
-    upload_to_obj_storage.call(
-        f"transcripts/{result_metadata['result_file_name']}", result_metadata['result_path']
-    )
-    upload_to_obj_storage.call(
-        f"transcripts/{result_metadata['result_vtt_name']}", result_metadata['result_vtt_path']
-    )
+        download_audio.call(job_spec.yt_video_url() if source.type ==
+                            'yt' else job_spec.twitch_video_url())
 
-    gc_artefacts.call()
+        input_raw_file_path = os.path.join(
+            config.CACHE_DIR, f"{source.id}.mp3")
+        result_path = os.path.join(
+            config.CACHE_DIR, f"{source.id}-{source.type}-result.json")
+
+        result_metadata: dict = do_transcribe.call(
+            input_raw_file_path,
+            model=job_spec.whisper_model,
+            result_path=result_path)
+
+        upload_to_obj_storage.call(
+            os.path.join('transcripts', result_metadata['result_file_name']),
+            result_metadata['result_path']
+        )
+        upload_to_obj_storage.call(
+            os.path.join('transcripts', result_metadata['result_vtt_name']),
+            result_metadata['result_vtt_path']
+        )
+
+    finally:
+        gc_artefacts.call()
+
     return result_metadata
 
 
@@ -220,7 +238,7 @@ def do_transcribe(input_audio_file_path: str, model: config.ModelSpec, result_pa
     logger.info(f"Finished downsampling file: {output_file}")
     logger.info(f"Preparing to transcribe file {output_file}")
 
-    segment_gen = split_silences(str(output_file))
+    segment_gen = split_silences(output_file)
 
     output_text = ""
     output_segments = []
@@ -233,13 +251,13 @@ def do_transcribe(input_audio_file_path: str, model: config.ModelSpec, result_pa
 
     result = {
         "text": output_text,
-        "segments": output_segments,
+        "segments": util.coalesce_short_transcript_segments(output_segments),
         "language": "en",
     }
 
     end_time = time.time()
 
-    logger.info(f"Writing openai/whisper transcription to {result_path}")
+    logger.info(f"Writing openai/whisper JSON transcription to {result_path}")
     with open(result_path, "w") as f:
         json.dump(result, f, indent=4)
 
@@ -253,12 +271,23 @@ def do_transcribe(input_audio_file_path: str, model: config.ModelSpec, result_pa
     logger.info(
         f"Finished transcribing file {input_audio_file_path} in {exec_time} seconds")
 
+    bucket_cdn_url = util.bucket_cdn_url(config.DO_DEFAULT_BUCKET_NAME)
+    result_json_name = os.path.basename(result_path)
+    result_vtt_name = os.path.basename(result_vtt_path)
+
     metadata = {
         'result_path': result_path,
         'result_vtt_path': result_vtt_path,
-        'result_file_name': os.path.basename(result_path),
-        'result_vtt_name': os.path.basename(result_vtt_path),
-        'src_audio_file_name': input_audio_file_path
+        'result_file_name': result_json_name,
+        'result_vtt_name': result_vtt_name,
+        'result_json_url': os.path.join(bucket_cdn_url, 'transcripts', result_json_name),
+        'result_vtt_url': os.path.join(bucket_cdn_url, 'transcripts', result_vtt_name),
+        'job_info': {
+            'exec_time_seconds': exec_time,
+            'model_name': model.name,
+            'start_time': start_time,
+            'end_time': end_time
+        }
     }
 
     return metadata
@@ -328,7 +357,6 @@ def upload_to_obj_storage(bucket_path: str, local_file_path: str, bucket_name=co
                         aws_access_key_id=os.environ["DO_SPACE_HOLOSAYS_KEY"],
                         aws_secret_access_key=os.environ["DO_SPACE_HOLOSAYS_SECRET"])
 
-    # Upload the JSON file to the Space
     s3.Object(bucket_name, bucket_path).put(
         Body=open(local_file_path, 'rb'), ACL='public-read')
 
